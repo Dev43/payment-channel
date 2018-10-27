d237fa75de1476b28792ec72dec6e3dfbc15e1ac
Dev43 <patrick.guay43@gmail.com>
2018-10-26 20:40:04 -0700

    add docs command and docs

---
+++ cmd/docs.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+func init() {
+	rootCmd.AddCommand(docCmd)
+}
+
+var docCmd = &cobra.Command{
+	Use:   "docs",
+	Short: "Generate markdown docs for payment_channel",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Generate markdown tree command dynamically
+		err := doc.GenMarkdownTree(cmd, "./docs")
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
